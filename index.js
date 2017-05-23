2867443420ec39c431f29c2fb65fb8159df61882
Joey Deeds <joey.deeds@gmail.com>
2017-05-22 21:34:57 -0500

    update index.js

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ var server = require('express');
 var app = server();
 var useragent = require('useragent');
 
-var port = process.env.PORT;
+var port = process.env.PORT || 3500;
 
-app.listen(port, function() {
-    console.log("Listening on port: " + port);
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
 });
 
-app.get('/', function(req,res) {
-    var agent = useragent.parse(req.headers['user-agent']);
+app.get('/', function(req, res) {
+  var agent = useragent.parse(req.headers['user-agent']);
 
-    var ipAdd = req.headers['x-fordwarded-for'];
-    var list = ipAdd.split('');
-    ipAdd = list[list.length - 1];
+  var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
 
-    res.json({
-        ip: ipAdd,
-        'language': req.headers['accept-language'].split(',')[0],
-        OS: agent.os.family,
-        device: agent.device.family
-    });
+  res.json({
+    ip: ipAddr,
+    "language": req.headers['accept-language'].split(',')[0],
+    OS: agent.os.family
+  });
 });
