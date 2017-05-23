e2589b3482fc8f75bf030d2ba68e681e35f76761
Joey Deeds <joey.deeds@gmail.com>
2017-05-22 21:19:11 -0500

    initial index.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # header-parser
 
+API for getting the IP Address, language and OS from the browser
--- index.js
+++ index.js
@@ -2,4 +2,23 @@ var server = require('express');
 var app = server();
 var useragent = require('useragent');
 
+var port = process.env.PORT;
 
+app.listen(port, function() {
+    console.log("Listening on port: " + port);
+});
+
+app.get('/', function(req,res) {
+    var agent = useragent.parse(req.headers['user-agent']);
+
+    var ipAdd = req.headers['x-fordwarded-for'];
+    var list = ipAdd.split('');
+    ipAdd = list[list.length - 1];
+
+    res.json({
+        ip: ipAdd,
+        'language': req.headers['accept-language'].split(',')[0],
+        OS: agent.os.family,
+        device: agent.device.family
+    });
+});
